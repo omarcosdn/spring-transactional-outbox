88fd54b32f56b6c3b35a509ae5d2f676cfc35b26
Marcos Demetrio <demetrius.marcos@gmail.com>
2023-10-31 23:46:59 -0300

    Create initial project

---
+++ src/main/java/io/github/omarcosdn/outbox/Main.java
@@ -0,0 +1,22 @@
+package io.github.omarcosdn.outbox;
+
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+
+  public static void main(String[] args) {
+    final var app = new SpringApplication(Main.class);
+    app.setBannerMode(Banner.Mode.OFF);
+    app.run(args);
+  }
+
+  @PostConstruct
+  void init() {
+    TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+  }
+}
